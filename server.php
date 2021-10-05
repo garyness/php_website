e875a3012423ec26ebeb16cfe9bb523dca9601e1
garyness <gary90326@gmail.com>
2021-10-05 10:44:40 +0800

    function Test

--- server.php
+++ server.php
@@ -1,4 +1,15 @@
 <?php
-    echo "hello world";
-    echo "Test code.";
+    class simpleobject{
+        public $user = "David";
+        public $id = 10001;
+
+        public function displayuserinfo(){
+            echo "The id of " + $this->user + "is " + $this->id + ".";
+        }
+    }
+
+    // $fun = new simpleobject();
+    // $fun->displayuserinfo();
+    echo "Hello world!";
+    echo $user;
 ?>
